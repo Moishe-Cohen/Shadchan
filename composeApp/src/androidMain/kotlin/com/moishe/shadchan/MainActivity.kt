d6e72ee8dce7456a57a44939502f07b0a13954da
Moishe-Cohen <fundconsultants@gmail.com>
2026-07-17 07:32:03 +0300

    Add files via upload

--- composeApp/src/androidMain/kotlin/com/moishe/shadchan/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/moishe/shadchan/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.remember
+import com.moishe.shadchan.platform.PlatformContext
 import com.moishe.shadchan.ui.ShadchanViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel = remember { ShadchanViewModel(applicationContext) }
+            val viewModel = remember { ShadchanViewModel(PlatformContext(applicationContext)) }
             App(viewModel)
         }
     }
